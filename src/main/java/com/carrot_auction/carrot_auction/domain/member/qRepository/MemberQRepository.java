e84d156362d46684be2c949757c2efd7288ae972
Dplo1514 <dladlsgur3334@gmail.com>
2023-07-26 15:49:26 +0900

    03_Feature 불필요 파일 제거작업

--- src/main/java/com/carrot_auction/carrot_auction/domain/member/qRepository/MemberQRepository.java
+++
@@ -1,5 +0,0 @@
-package com.carrot_auction.carrot_auction.domain.member.qRepository;
-
-public interface MemberQRepository {
-
-}
--- src/main/java/com/carrot_auction/carrot_auction/domain/member/repository/AuctionSubscribeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carrot_auction.carrot_auction.domain.member.repository;
-
-import com.carrot_auction.carrot_auction.domain.member.entity.AuctionSubscribe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuctionSubscribeRepository extends JpaRepository<AuctionSubscribe, String> {
-}
